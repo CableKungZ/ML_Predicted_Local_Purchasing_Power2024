8d6d96a615d3f22ec3335cbffe0cc3c28198ef69
CableKungZ <xdredkillgamertvchannel@gmail.com>
2024-09-12 14:20:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 model = joblib.load('models/agg.pkl')
 
 # สร้างอินเตอร์เฟซ Streamlit
-st.title('Cost of Living Prediction')
+st.title('Unsupervised Learning Model Prediction')
 
 # สร้างช่องรับอินพุตสำหรับดัชนีต่าง ๆ
 cost_of_living_index = st.number_input('Cost of Living Index', min_value=0.0, max_value=1000.0, value=50.0)
@@ -16,12 +16,12 @@ restaurant_price_index = st.number_input('Restaurant Price Index', min_value=0.0
 local_purchasing_power_index = st.number_input('Local Purchasing Power Index', min_value=0.0, max_value=1000.0, value=50.0)
 
 # เมื่อผู้ใช้กดปุ่มทำนาย
-if st.button('Predict'):
+if st.button('Predict Cluster'):
     # เตรียมข้อมูลอินพุต
     input_data = np.array([[cost_of_living_index, rent_index, groceries_index, restaurant_price_index, local_purchasing_power_index]])
     
     # ใช้โมเดลในการทำนายผล
-    result = model.predict(input_data)
+    cluster = model.fit_predict(input_data)
     
     # แสดงผลลัพธ์การทำนาย
-    st.write('Prediction result:', result[0])
+    st.write('Predicted Cluster:', cluster[0])
